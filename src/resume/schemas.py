28b4b89bddb315b1eaead41e324f92208e8d40ff
dmitriy levykin <dmlegasy@gmail.com>
2026-05-01 07:03:58 +0300

    refactor: update schemas

--- src/resume/schemas.py
+++ src/resume/schemas.py
@@ -1,26 +1,16 @@
-from ninja import ModelSchema, Schema
-from pydantic import EmailStr, ConfigDict
+from ninja import ModelSchema
 
 from resume.models import Resume
 
 
-# class ResumeSchema(ModelSchema):
-#     class Meta:
-#         model = Resume
-#         exclude = "id",
-        
-class ResumeSchemaD(Schema):
-    title: str = None
-    phone_number: str = None
-    email: EmailStr = None
-    experience: str = None
-    education: str = None
+class ResumeIn(ModelSchema):
+    class Meta:
+        model = Resume
+        exclude = "id",
+        fields_optional = "__all__"
 
 
-class ResumeSchemaR(Schema):
-    id: int
-    title: str
-    phone_number: str
-    email: EmailStr
-    experience: str
-    education: str
+class ResumeOut(ModelSchema):
+    class Meta:
+        model = Resume
+        fields = "__all__"
--- src/resume/views.py
+++ src/resume/views.py
@@ -3,42 +3,42 @@ from django.shortcuts import aget_object_or_404, aget_list_or_404
 from ninja import Router
 
 from resume.models import Resume
-from resume.schemas import ResumeSchemaD, ResumeSchemaR
+from resume.schemas import ResumeIn, ResumeOut
 
 router = Router(tags=["Resume"])
 
 
 @router.post("/resume")
-async def create_resume(request: HttpRequest, resume_schema: ResumeSchemaD):
+async def create_resume(request: HttpRequest, resume_schema: ResumeIn):
     """Create a new resume."""
     resume = await Resume.objects.acreate(**resume_schema.dict())
     return JsonResponse(data={"status": True, "resume_id": resume.id}, status=201)
 
 
 @router.get("/resume")
-async def get_resume(request: HttpRequest) -> list[ResumeSchemaR]:
+async def get_resume(request: HttpRequest) -> list[ResumeOut]:
     """Get all resumes."""
     resume_orm = await aget_list_or_404(Resume)
-    resume_schemas = [ResumeSchemaR.from_orm(resume) for resume in resume_orm]
+    resume_schemas = [ResumeOut.from_orm(resume) for resume in resume_orm]
     return resume_schemas
 
 
 @router.get("/resume/{resume_id}")
-async def get_resume_by_id(request: HttpRequest, resume_id: int) -> ResumeSchemaR:
+async def get_resume_by_id(request: HttpRequest, resume_id: int) -> ResumeOut:
     """Get a resume by id."""
     resume = await aget_object_or_404(Resume, id=resume_id)
-    return ResumeSchemaR.from_orm(resume)
+    return ResumeOut.from_orm(resume)
 
 
 @router.put("/resume/{resume_id}")
-async def update_resume(request: HttpRequest, resume_id: int, resume_schema: ResumeSchemaD) -> dict:
+async def update_resume(request: HttpRequest, resume_id: int, resume_schema: ResumeIn) -> dict:
     """Full update a resume."""
     await Resume.objects.filter(id=resume_id).aupdate(**resume_schema.dict())
     return {"success": True}
 
 
 @router.patch("/resume/{resume_id}")
-async def patch_resume(request: HttpRequest, resume_id: int, resume_schema: ResumeSchemaD) -> dict:
+async def patch_resume(request: HttpRequest, resume_id: int, resume_schema: ResumeIn) -> dict:
     """Partial update a resume."""
     resume_data = resume_schema.dict(exclude_unset=True)
     await Resume.objects.filter(id=resume_id).aupdate(**resume_data)
